f5eb9254aa4b40a8500d5da4da96ebb2842c5ae1
Tharsanan <96085291+tharsananarul@users.noreply.github.com>
2026-04-24 00:20:33 +0200

    Add automated image optimization to the build process (65% reduction)

--- portfolio-react/vite.config.js
+++ portfolio-react/vite.config.js
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      jpg: {
+        quality: 70,
+      },
+      jpeg: {
+        quality: 70,
+      },
+      png: {
+        quality: 70,
+      },
+      webp: {
+        lossless: true,
+      },
+    }),
+  ],
   base: '/',
 })
